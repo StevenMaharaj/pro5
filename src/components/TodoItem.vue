35dbf1686fb7a1d2ba20b3b27faf1cf8d948bfbd
StevenMaharaj <stevesamah@gmail.com>
2020-04-08 12:39:24 +1000

    inti commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="todo-item" :class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" @change="markComplete" />
+      {{todo.title}}
+      <button @click="$emit('del-todo',todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background-color: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #cccccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background-color: #ff0000;
+  color: #fff;
+  float: right;
+  border-radius: 2rem;
+}
+</style>
